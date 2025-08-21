37910ad977495ff1d167d3033c63135931f0c63c
rosandazivanovic <rosa.zivanovic03@gmail.com>
2025-08-21 15:26:52 +0200

    11

--- client/src/components/auth/PrijavaForma.tsx
+++ client/src/components/auth/PrijavaForma.tsx
@@ -31,7 +31,16 @@ export function PrijavaForma({ authApi }: AuthFormProps) {
 
   return (
     <div className="bg-white/30 backdrop-blur-lg shadow-md rounded-2xl p-10 w-full max-w-md border border-blue-400">
-      <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">Prijava</h1>
+      {/* Naslov */}
+      <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
+        Prijava
+      </h1>
+
+      {/* Akademski opis */}
+      <p className="text-center text-gray-600 mb-6 max-w-md mx-auto">
+        Da biste koristili sistem za evidenciju i upravljanje prijavama i kvarovima, molimo prijavite se.
+      </p>
+
       <form onSubmit={podnesiFormu} className="space-y-4">
         <input
           type="text"
@@ -50,14 +59,14 @@ export function PrijavaForma({ authApi }: AuthFormProps) {
         {greska && <p className="text-md text-center text-red-700/80 font-medium">{greska}</p>}
         <button
           type="submit"
-          className="w-full bg-blue-700/70 hover:bg-blue-700/90 text-white py-2 rounded-xl transition"
+          className="w-full bg-black-700/70 hover:bg-blue-700/90 text-white py-2 rounded-xl transition"
         >
           Prijavi se
         </button>
       </form>
       <p className="text-center text-sm mt-4">
         Nemate nalog?{" "}
-        <Link to="/register" className="text-blue-700 hover:underline">
+        <Link to="/register" className="text-grey-900 hover:underline font-medium">
           Registrujte se
         </Link>
       </p>
